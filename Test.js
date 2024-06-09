1f9e1280ec92168d4f08d381139c819acd58f465
PlayGround <chavdadev294@gmail>
2024-06-09 12:59:02 +0530

    javaScript

--- Test.js
+++ Test.js
@@ -1,1 +1,15 @@
-console.log("Dev");
+const accountId=10
+let accountEmail="abc@gmail.com"
+var accountPassword="12345"
+accountCity="Pune"
+
+let acoountstate;
+
+accountEmail="xyz@gmail.com"
+accountPassword="13345"
+accountCity="Goa"
+console.log(accountId);
+
+//Don't use var 
+
+console.table([acoountstate,accountId,accountEmail,accountPassword,accountCity])
---
+++ Test1.js
@@ -0,0 +1,17 @@
+let name="Dev"
+
+let age=88;
+let isloggedIn=false
+
+//number => 2^53 - 1
+// console.log(Number.MAX_SAFE_INTEGER);
+
+//bigint
+//string=> ""
+//boolean =>true/false
+//null => stand alone Value
+//Undefined =>not assigned
+//symbol => unique
+//Object
+console.log(typeof undefined);
+console.log(typeof null);
