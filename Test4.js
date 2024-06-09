3a96948ca77435069b55cab0c0dd036de05987c1
PlayGround <chavdadev294@gmail>
2024-06-09 17:31:18 +0530

    javaScript

--- Test4.js
+++ Test4.js
@@ -1,8 +1,14 @@
-let name0="Dev"
-let name1=name0
-console.log(name1);
+// let name0="Dev"
+// let name1=name0
+// console.log(name1);
 
-let user={
-    email: "abc@gmail.com",
-    upi:"user@ybl"
-}
+// let user={
+//     email: "abc@gmail.com",
+//     upi:"user@ybl"
+// }
+
+
+
+const name1 = "Dev";
+const repocount = 50;
+console.log(`hello my name is ${name1} and my repocount is ${repocount}`);
