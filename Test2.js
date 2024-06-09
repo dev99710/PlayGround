283d3aabba8c899ab84a4555d7b5e5e1d09347b2
PlayGround <chavdadev294@gmail>
2024-06-09 13:35:47 +0530

    javaScript

---
+++ Test2.js
@@ -0,0 +1,12 @@
+let score=null
+// check using 3abc,null,undefined
+console.log(typeof score);
+let value=Number(score)
+
+console.log(value);
+console.log(typeof value);
+
+//"33"=>33
+//"3abc"=>NaN
+//true =>1 and false=>0
+
