95c07453511bed87d3e054715d7b51474c897211
PlayGround <chavdadev294@gmail>
2024-06-09 16:37:15 +0530

    javaScript

--- Test3.js
+++ Test3.js
@@ -5,3 +5,16 @@
 // console.log(null==0);
 // console.log(null >=0);
 
+const heros=["Superman","Spiderman","widow"]
+
+let my={
+    name: "boom",
+    age:112
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+console.log(typeof heros);
