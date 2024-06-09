a6ce56853b79e40fa4feb7387ef0c1953ee0bcd8
PlayGround <chavdadev294@gmail>
2024-06-09 17:48:15 +0530

    Javascript

--- Test4.js
+++ Test4.js
@@ -12,3 +12,28 @@
 const name1 = "Dev";
 const repocount = 50;
 console.log(`hello my name is ${name1} and my repocount is ${repocount}`);
+
+
+const Game=new String("Micy-Mouse");
+console.log(Game);
+// console.log(Game.__proto__);
+
+
+console.log(Game.toUpperCase());
+console.log(Game.length);
+console.log(Game.charAt(6));
+console.log(Game.indexOf('o'));
+
+const new1 = Game.substring(0,5);
+console.log(new1);
+
+
+const anotherString=Game.slice(-8,10)
+console.log(anotherString);
+
+
+const NewString1="   Dev   "
+console.log(NewString1);
+console.log(NewString1.trim());
+
+const url="https://Dev.com/DevChavda"
