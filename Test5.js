8c7aebb3c8b41496d4081062a2cf414ce9841f27
PlayGround <chavdadev294@gmail>
2024-06-10 23:48:05 +0530

    JavaScript

---
+++ Test5.js
@@ -0,0 +1,35 @@
+const { logEvent } = require("firebase/analytics")
+
+const score=400
+
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const OtherNumber=123.8966
+const OtherNumber1=1123.8966
+
+// console.log(OtherNumber.toPrecision(3));
+// console.log(OtherNumber1.toPrecision(3));//😆 use 4
+
+const Hun=1000000
+// console.log(Hun.toLocaleString());
+
+// console.log(Math);
+// console.log(Math.abs(-4));//Positive
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));//Max Value
+// console.log(Math.floor(4.9));//Lowest Value
+
+// console.log(Math.min(55,57,98,70));
+
+console.log(Math.random()*10+1);
+
+
+
+const Max=20
+const Min=10
+
+console.log(Math.floor(random()*(Max-Min+1))+Min)
